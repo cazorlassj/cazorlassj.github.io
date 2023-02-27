ef0a21d6607114b496b76878837ae874173a5ed0
cazorla <51689868+cazorlassj@users.noreply.github.com>
2023-02-27 12:17:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+  var aboveHeight = $('nav').outerHeight();
+  
+  $(window).scroll(function(){
+       if ($(window).scrollTop() > aboveHeight){
+
+            $('nav').addClass('fixed').css('top','0').next().css('padding-top','20px');
+
+       } else {
+           $('nav').removeClass('fixed').next().css('padding-top','0');
+      }
+  });
+  
   $("a").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
