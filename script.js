963a540246759006fc5f949d236556ec5495771e
cazorla <51689868+cazorlassj@users.noreply.github.com>
2023-02-27 12:34:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-  var aboveHeight = $('nav').outerHeight();
-  
-  $(window).scroll(function(){
-       if ($(window).scrollTop() > aboveHeight){
-
-            $('nav').addClass('fixed').css('top','0').next().css('padding-top','20px');
-
-       } else {
-           $('nav').removeClass('fixed').next().css('padding-top','0');
-      }
-  });
-  
   $("a").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
@@ -18,8 +6,22 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 900, function(){
+
         window.location.hash = hash;
       });
     }
   });
+  
+  var aboveHeight = $('nav').outerHeight();
+
+    $(window).scroll(function(){
+
+        if ($(window).scrollTop() > aboveHeight){
+
+            $('nav').addClass('fixed').css('top','0').next().css('padding-top','20px');
+
+        } else {
+            $('nav').removeClass('fixed').next().css('padding-top','0');
+        }
+    });
 });
